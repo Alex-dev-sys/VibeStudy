fd3a7e118ed68af6da2bbe8464116aa9ca121754
Alex-dev-sys <aleksei.kolganov.2019@gmail.com>
2025-11-15 15:25:08 +0300

    remove: удалена система погоды

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -78,7 +78,6 @@ export const viewport: Viewport = {
 
 import { OnboardingProvider } from '@/components/onboarding/OnboardingProvider';
 import { RealtimeProvider } from '@/components/realtime/RealtimeProvider';
-import { WeatherSystem } from '@/components/weather';
 import { Toaster } from 'sonner';
 
 export default function RootLayout({
@@ -95,9 +94,6 @@ export default function RootLayout({
               Перейти к основному содержимому
             </a>
             {children}
-            <div className="fixed top-4 right-4 z-50">
-              <WeatherSystem />
-            </div>
             <Toaster position="bottom-right" />
           </OnboardingProvider>
         </RealtimeProvider>
--- src/components/weather/index.ts
+++
@@ -1,5 +0,0 @@
-export { WeatherSystem } from './WeatherSystem';
-export { WeatherToggle } from './WeatherToggle';
-export { WeatherScene } from './WeatherScene';
-export { Sun } from './Sun';
-export { ParticleSystem } from './ParticleSystem';
