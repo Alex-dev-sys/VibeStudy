a072b4e8ed1fe62544595c64f729fe8898708412
Alex-dev-sys <aleksei.kolganov.2019@gmail.com>
2025-12-01 00:19:14 +0300

    Add tsconfig-paths and configure ts-node for CommonJS

--- scripts/start-bot.js
+++ scripts/start-bot.js
@@ -1,5 +1,20 @@
 /**
  * Entry point for PM2 to run the TypeScript bot
  */
-require('ts-node/register');
+const tsConfig = require('../tsconfig.json');
+const tsConfigPaths = require('tsconfig-paths');
+
+// Register tsconfig-paths
+tsConfigPaths.register({
+    baseUrl: tsConfig.compilerOptions.baseUrl || '.',
+    paths: tsConfig.compilerOptions.paths || {}
+});
+
+// Register ts-node with CommonJS to avoid ESM issues
+require('ts-node').register({
+    compilerOptions: {
+        module: 'commonjs'
+    }
+});
+
 require('./telegram-bot-polling.ts');
